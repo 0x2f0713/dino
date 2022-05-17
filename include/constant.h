97677dfe5d3597c4d7131a3206a48e1a10c1fa84
0x2f0713 <namhaiha0308@gmail.com>
2022-05-17 18:31:17 +0700

    add dino related constant and utilities function

--- include/constant.h
+++ include/constant.h
@@ -3,8 +3,21 @@
 
 #define SCREEN_WIDTH 1366
 #define SCREEN_HEIGHT 768
-#define WINDOW_TITLE "Chess"
+#define WINDOW_TITLE "Dino"
+
+#define CENTER_HORIZONTAL(x) SCREEN_WIDTH  / 2 - x / 2
+#define CENTER_VERTICAL(x) SCREEN_HEIGHT / 2 - x / 2
+
+
 #define V_JUMP -7
 #define ACCELERATION 1
-#define RANDOM_ENEMY rand() % 10 + (1) * 100;
+#define RANDOM_ENEMY_INTERVAL rand() % 10 + (1) * 100;
+
+#define TRASH_PIXEL 20
+
+#define DINO_STATUS_RUNNING 0
+#define DINO_STATUS_JUMPING 1
+#define DINO_STATUS_DIE     2
+
+
 #endif
