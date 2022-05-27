9b2424f05a875bbc5ad35fe14067189e3abfb9d3
0x2f0713 <namhaiha0308@gmail.com>
2022-05-27 10:33:12 +0700

    remove unused code

--- src/screen/lobby.cpp
+++ src/screen/lobby.cpp
@@ -10,29 +10,12 @@
 
 #include "include/screen/lobby.h"
 
-std::map<std::string, std::string> mediaLists{
-    {"DINO-DOWN", "resources/images/object/dino-down.png"},
-    {"DINO-UP", "resources/images/object/dino-up.png"}};
 
 void initScreen(SDL_Window *window, SDL_Renderer *renderer, TTF_Font *font)
 {
   // Fill the window
   SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
   SDL_RenderClear(renderer);
-
-  // UI::showObject(renderer, object::DINO_NORMAL);
-
-  // SDL_Texture *texture = UI::loadMedia(mediaLists["DINO-UP"], renderer);
-  // SDL_Rect image_src = {0, 0, 100, 100};
-  // SDL_Rect image_dest = {160, SCREEN_HEIGHT / 3 * 2 - 160, 160, 160};
-  // SDL_RenderCopy(renderer, texture, NULL, &image_dest);
-
-  // SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  // SDL_RenderDrawLine(renderer, 0,SCREEN_HEIGHT / 3 * 2-10,SCREEN_WIDTH, SCREEN_HEIGHT / 3 * 2);
-  // // Test write text
-  // SDL_Color color = {0, 0, 0};
-  // SDL_Rect dest = {SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/2, 0, 0};
-  // UI::showText(renderer, font, "Press Arrow Up button to Play", color, &dest);
 
   UI::showObject(renderer, object::DINO_NORMAL);
   SDL_Color color = {0, 0, 0};
